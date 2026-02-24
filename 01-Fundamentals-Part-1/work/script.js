386589144399fb5dece5c1ae5363773e4dd618dd
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2026-02-24 17:13:02 +0530

    feat: assignment Values and Variables

--- 01-Fundamentals-Part-1/work/script.js
+++ 01-Fundamentals-Part-1/work/script.js
@@ -1,23 +1,36 @@
-let js = "amazing";
+// lecture 8
+// let js = "amazing";
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
+// ######################################
 
-let firstName = "Matilda";
+// lecture 9
+// let firstName = "Matilda";
+//
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let jonasMatilda = "JM";
+// let $function = 27;
 
-let jonasMatilda = "JM";
-let $function = 27;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let person = "jonas";
-let PI = 3.1415;
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+// console.log(myFirstJob);
+// #######################################
 
-console.log(myFirstJob);
+// lecture 10
+let country = "India";
+let continent = "Asia";
+let population = 1470;
+
+console.log(country);
+console.log(continent);
+console.log(population + " million");
