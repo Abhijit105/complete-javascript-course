59547c57ef53b1303794f2dd4b8b0910616bc2ea
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2026-02-25 21:31:00 +0530

    feat: lecture 13

--- 01-Fundamentals-Part-1/work/script.js
+++ 01-Fundamentals-Part-1/work/script.js
@@ -60,15 +60,46 @@
 // ########################################
 
 // lecture 12
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1989;
-// birthYear = 1990; // --- ERROR
-// const job; // --- ERROR
+// const birthYear = 1989;
+// // birthYear = 1990; // --- ERROR
+// // const job; // --- ERROR
 
-var job = "programmer";
-job = "teacher";
+// var job = "programmer";
+// job = "teacher";
 
-lastName = "Kumar";
-console.log(lastName);
+// lastName = "Kumar";
+// console.log(lastName);
+// ########################################
+
+// lecture 13
+const now = 2037;
+
+const ageAbhijit = now - 1989;
+
+const ageSarah = now - 2018;
+console.log(ageAbhijit, ageSarah);
+
+console.log(ageAbhijit * 2, ageAbhijit / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8
+
+const firstName = "Abhijit";
+const lastName = "Kumar";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// comparison operators
+console.log(ageAbhijit > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1989 > now - 2018);
