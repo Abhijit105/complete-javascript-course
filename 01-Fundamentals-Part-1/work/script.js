e466b94125e2f914b4cc13218617bbbb96790d8d
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2026-02-25 18:35:55 +0530

    feat: lecture 12

--- 01-Fundamentals-Part-1/work/script.js
+++ 01-Fundamentals-Part-1/work/script.js
@@ -37,23 +37,38 @@
 // #######################################
 
 // lecture 11
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof "Jonas");
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
 
-javascriptIsFun = "YES!";
-console.log(javascriptIsFun);
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(year);
-console.log(typeof year);
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
 
-console.log(typeof null); // --- IGNORE ---
+// console.log(typeof null); // --- IGNORE
+// ########################################
+
+// lecture 12
+let age = 30;
+age = 31;
+
+const birthYear = 1989;
+// birthYear = 1990; // --- ERROR
+// const job; // --- ERROR
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Kumar";
+console.log(lastName);
