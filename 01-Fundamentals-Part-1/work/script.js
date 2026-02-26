3462b8e195706fa923f468a861ecd5ea257c9997
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2026-02-26 23:56:59 +0530

    feat: add operator precendence

--- 01-Fundamentals-Part-1/work/script.js
+++ 01-Fundamentals-Part-1/work/script.js
@@ -75,31 +75,50 @@
 // ########################################
 
 // lecture 13
+// const now = 2037;
+
+// const ageAbhijit = now - 1989;
+
+// const ageSarah = now - 2018;
+// console.log(ageAbhijit, ageSarah);
+
+// console.log(ageAbhijit * 2, ageAbhijit / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8
+
+// const firstName = "Abhijit";
+// const lastName = "Kumar";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// // comparison operators
+// console.log(ageAbhijit > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1989 > now - 2018);
+// ########################################
+
+// lecture 14
 const now = 2037;
 
 const ageAbhijit = now - 1989;
-
 const ageSarah = now - 2018;
 console.log(ageAbhijit, ageSarah);
 
-console.log(ageAbhijit * 2, ageAbhijit / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8
-
-const firstName = "Abhijit";
-const lastName = "Kumar";
-console.log(firstName + " " + lastName);
-
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--; // x = x - 1
-console.log(x);
-
-// comparison operators
-console.log(ageAbhijit > ageSarah); // >, <, >=, <=
-console.log(ageSarah >= 18);
-
-const isFullAge = ageSarah >= 18;
-
 console.log(now - 1989 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageAbhijit + ageSarah) / 2;
+console.log(ageAbhijit, ageSarah, averageAge);
